d0a8a7209087f3bc74d1cfffc6ae0099802d9225
mirabelkoso <mirabelkoso@gmail.com>
2015-01-16 13:26:46 +0100

    allowing access

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
 
     // intercept OPTIONS method
     // if ('OPTIONS' == req.method) {
